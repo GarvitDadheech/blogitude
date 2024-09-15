ad639ba71f365328ece0939fa31d88e4e4238a96
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-09-15 18:40:36 +0530

    store inputs in signin

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,15 +1,31 @@
+import { useState } from "react"
 import { Button } from "../components/Button"
 import { Heading } from "../components/Heading"
 import { InputBox } from "../components/InputBox"
 import { Quote } from "../components/Quote"
+import { SignInBody } from "@garvit_dadheech/blogitude"
 
 export const Signin = () => {
+    const [postInputs,setPostInputs] = useState<SignInBody>({
+        email: "",
+        password: ""
+    })
     return (
         <div className="grid grid-cols-1 lg:grid-cols-2">
             <div className="flex flex-col justify-center items-center">
                 <Heading headingContent="Enter Your Credentials" subHeadingContent="Don't have an Account?" isSignUp={false}/>
-                <InputBox label="Email" placeholder="garvit@gmail.com" onChange={() => console.log("changed")}/>
-                <InputBox label="Password" placeholder="" onChange={() => console.log("changed")} type="password"/>
+                <InputBox label="Email" placeholder="garvit@gmail.com" onChange={(e) => {
+                    setPostInputs({
+                        ...postInputs,
+                        email: e.target.value
+                    })
+                }}/>
+                <InputBox label="Password" placeholder="" onChange={(e) => {
+                    setPostInputs({
+                        ...postInputs,
+                        password: e.target.value
+                    })
+                }} type="password"/>
                 <Button content="Sign In"/>
             </div>
             <Quote/>
