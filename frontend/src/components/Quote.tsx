b3e3bbca8554d1e7103f7d3da8a60502c0775c24
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-09-15 16:41:43 +0530

    add quote component

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,13 @@
+export const Quote = () => {
+   return(
+    <div className="bg-slate-100 h-screen flex items-center justify-center">
+        <div className="flex flex-col items-center justify-center">
+            <p className="text-4xl font-bold w-2/3">"Write what you care about. Write what matters to you. The world can tell when you are passionate."</p>
+            <div className="w-2/3">
+                <p className="mt-5 text-2xl font-semibold">Jon Morrow</p>
+                <p className="text-slate-700">CEO, Smart Blogger</p>
+            </div>
+        </div>
+    </div>
+   )
+}
--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,5 +1,12 @@
+import { Quote } from "../components/Quote"
+
 export const Signin = () => {
     return (
-        <div>Signin</div>
+        <div className="grid grid-cols-2">
+            <div className="">
+
+            </div>
+            <Quote/>
+        </div>
     )
 } 
